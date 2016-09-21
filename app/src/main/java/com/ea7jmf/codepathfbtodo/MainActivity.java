6e768f14af503237efe2b52182f8de859d2dd7f6
Jesus F. Trujillo <jesusft@yahoo-inc.com>
2016-09-20 21:06:00 -0700

    support for adding, removing and persisting items

---
+++ app/src/main/java/com/ea7jmf/codepathfbtodo/MainActivity.java
@@ -0,0 +1,75 @@
+package com.ea7jmf.codepathfbtodo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> todoItems;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
+    EditText etEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        todoItems = new ArrayList<>();
+        populateArrayItems();
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        lvItems.setAdapter(itemsAdapter);
+        etEditText = (EditText) findViewById(R.id.etEditText);
+
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                todoItems.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                writeItems();
+                return true;
+            }
+        });
+    }
+
+    private void populateArrayItems() {
+        readItems();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File file = new File(filesDir, "todo.txt");
+        try {
+            todoItems = new ArrayList<String>(FileUtils.readLines(file));
+        } catch (IOException e) {
+
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File file = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(file, todoItems);
+        } catch (IOException e) {
+
+        }
+    }
+
+    public void onAddItem(View view) {
+        itemsAdapter.add(etEditText.getText().toString());
+        etEditText.setText("");
+        writeItems();
+    }
+}
